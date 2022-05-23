54e8f72df17384537424236027548780faaf78a4
오세연 <105721930+comehereplz@users.noreply.github.com>
2022-05-23 20:39:37 +0900

    trafficLights + bluetooth

--- trafficLights.ino
+++ trafficLights.ino
@@ -1,28 +1,47 @@
-#define LED_RED 3       // 빨간불 핀 번호 3
-#define LED_YELLOW 4    // 노란불 핀 번호 4
-#define LED_GREEN 5     // 초록불 핀 번호 5
+#define LED_RED 13       // 빨간불 핀 번호 13
+#define LED_YELLOW 12    // 노란불 핀 번호 12
+#define LED_GREEN 11     // 초록불 핀 번호 11
+
+#include <SoftwareSerial.h>   // SoftwareSerial을 통해 블루투스와 데이터 전송
+
+SoftwareSerial BTserial(2,3);  // Rx, Tx
+
+int result = 1;
 
 void setup() {
   pinMode(LED_RED, OUTPUT);
   pinMode(LED_YELLOW, OUTPUT);
   pinMode(LED_GREEN, OUTPUT);
+
+  Serial.begin(9600);     // 9600 속도로 시리얼 통신을 시작한다
+  BTserial.begin(9600);   // 9600 속도로 블루투스 시리얼 통신을 시작한다
 }
 
 void loop() {
   turnOffAll();     // LED 다 끕니다.
 
-  digitalWrite(LED_RED, HIGH);   // 빨간불 켜기
-  delay(3000);    // 3초 간 유지
-  turnOffAll();   // 불 다 끄기
-
-  digitalWrite(LED_GREEN, HIGH);    // 초록불 켜기
-  delay(3000);    // 3초 간 유지
-  turnOffAll();   // 불 다 끄기
-
-  digitalWrite(LED_YELLOW, HIGH);   // 노란불 켜기
-  delay(2000);    // 2초 간 유지
-  turnOffAll();   // 불 다 끄기
-}
+    if(result == 1) {
+      digitalWrite(LED_RED, HIGH);
+      Serial.println("RED");
+      delay(7000);
+      turnOffAll();
+      result = 2;
+    }
+    else if(result == 2) {
+      digitalWrite(LED_YELLOW, HIGH);
+      Serial.println("YELLOW");
+      delay(1000);
+      turnOffAll();
+      result = 3;
+    }
+    else if(result == 3) {
+      digitalWrite(LED_GREEN, HIGH);
+      Serial.println("GREEN");
+      delay(7000);
+      turnOffAll();
+      result = 1;
+    }
+ }
 
 void turnOffAll() {     // turnOffAll 함수 정의
   digitalWrite(LED_RED, LOW);   // 빨간불 끄기

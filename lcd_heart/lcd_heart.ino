bb2cb41e0085d341ffa717b6c5360b4ad1883669
오세연 <105721930+comehereplz@users.noreply.github.com>
2022-05-31 11:27:23 +0900

    LCD
    LCD에 그림 출력하기

---
+++ lcd_heart/lcd_heart.ino
@@ -0,0 +1,17 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd {2, 3, 4, 5, 6, 7};
+
+uint8_t smile[8] = {0x00, 0x0A, 0x1F, 0x0E, 0x04, 0x00, 0x00};
+
+void setup() {
+  lcd.createChar(1, smile); // 생성 문자를 CGRAM 1에 씀
+
+  lcd.begin(16, 2);
+  lcd.setCursor(8, 0); // 문자 표시 위치 지정
+  lcd.write(1);
+}
+
+void loop() {
+  // 아무것도 하지 않음
+}
